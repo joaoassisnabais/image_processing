277a553b1ce184a002271b8d9fcbaa6ba2892f78
Clara-S-Pais <clara.s.pais@tecnico.ulisboa.pt>
2023-12-06 15:03:16 +0000

    map bug? fix

--- src/compute_transform.py
+++ src/compute_transform.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 from utils import read
 from utils.feat_manipulation import feat_matching, RANSAC
-from utils.debug_funcs import show_image_and_features, get_single_frame, show_homogaphies, compare_process_video_sift, check_if_drawmatches_works
+from utils.debug_funcs import show_image_and_features, get_single_frame, show_homogaphies, compare_process_video_sift
 from utils.homography import homography
 
 debug = False
@@ -31,6 +31,9 @@ def main(config_file):
 
     transforms_out_all = np.empty((0,11))
     store_H = [np.identity(3)]  # index k -> homography between frame k and previus frame
+    store_k_to_map = [np.identity(3)] # index k -> homography between frame k and the map
+
+    store_matches = [[0,0],]  # used for debug. index k -> feature matches between frame k and the previus frame
     
     for k in range(1, len(feat)):
         transforms_out = np.array([])
@@ -55,10 +58,13 @@ def main(config_file):
 
             frames = [get_single_frame("src/data/backcamera_s1.mp4", k), get_single_frame("src/data/backcamera_s1.mp4", k-1)]
             img = cv.drawMatchesKnn(frames[0], tuple(kp1), frames[1], tuple(kp2), match1to2_cv, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-            plt.imshow(img)
-            plt.show
+            #plt.imshow(img)
+            #plt.show
         
         matches1, matches2, mask = RANSAC(matches1, matches2)
+
+        if debug:
+            store_matches += [[matches1,matches2]]
         
         show_image_and_features("src/data/backcamera_s1.mp4", k, k-1, matches1, matches2) # show the keypoints to make sure they make sense
         
@@ -69,10 +75,13 @@ def main(config_file):
             #obtain the homography from current frame to map from the homography from current frame to previous frame
             if(k==1):
                 store_H += [np.copy(H)]
+                store_k_to_map += [np.copy(H)]
             else:
                 store_H += [np.copy(H)]
-                H = np.matmul(H, np.linalg.inv(store_H[k-1]))
+                H = np.matmul(store_k_to_map[k-1], H) #np.matmul(H, np.linalg.inv(store_H[k-1]))
+                store_k_to_map += [np.copy(H)]
                 print(len(store_H),k-1)
+
         elif map_or_all == 'all':
 
             store_H += [np.copy(H)]
@@ -80,7 +89,7 @@ def main(config_file):
             print("bad config. file: transforms must be map or all")
 
         if map_or_all == 'map':
-            show_homogaphies(matches1, matches2, H, "src/data/backcamera_s1.mp4", 0, k)
+            show_homogaphies(H, "src/data/backcamera_s1.mp4", k, 0)
 
             transforms_out = np.concatenate((transforms_out, np.asarray([0,k])))
             transforms_out = np.concatenate((transforms_out, H.reshape(9)))
@@ -92,8 +101,6 @@ def main(config_file):
             
             for j in range(i+1, len(store_H)):
                 H = np.matmul(H, np.linalg.inv(store_H[k-1]))
-
-                show_homogaphies(matches1, matches2, H, "src/data/backcamera_s1.mp4", 0, k)
 
                 transforms_out = np.array([])
                 transforms_out = np.concatenate((transforms_out, np.asarray([i,j])))
@@ -110,5 +117,5 @@ def main(config_file):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1])
     
--- src/utils/debug_funcs.py
+++ src/utils/debug_funcs.py
@@ -1,6 +1,7 @@
 import cv2
 from matplotlib import pyplot as plt
 from scipy.io import loadmat, savemat
+import numpy as np
 
 
 def get_single_frame(file_path, frame_number):
@@ -80,7 +81,7 @@ def show_image_and_features(video_path, index1, index2, keypoints1, keypoints2):
     plt.show()
 
 
-def show_homogaphies(src, dest, H, video_path, index1, index2):
+def show_homogaphies(H, video_path, index1, index2):
     """
     Given 2 frames, show the difference between the homography computed by
     our algorithm and the homography computed by opencv.
@@ -100,13 +101,35 @@ def show_homogaphies(src, dest, H, video_path, index1, index2):
     index2 : int
         Index of the second frame.
     """
-    Hgood, _ = cv2.findHomography(src, dest)
+    
 
     image = get_single_frame(video_path,index1)
     image2 = get_single_frame(video_path,index2)
 
+    sift = cv2.SIFT_create(2000)
+    keypoints1, descriptors1 = sift.detectAndCompute(image, None)
+    keypoints2, descriptors2 = sift.detectAndCompute(image2, None)
+
+
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+
+
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good_matches.append(m)
+
+
+    src_pts = np.float32([keypoints1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([keypoints2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+
+    homography_matrix, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+
     transformed_image = cv2.warpPerspective(image, H, (image.shape[1], image.shape[0]))
-    transformed_imageCV = cv2.warpPerspective(image, Hgood, (image.shape[1], image.shape[0]))
+    transformed_imageCV = cv2.warpPerspective(image, homography_matrix, (image.shape[1], image.shape[0]))
 
     fig, axes = plt.subplots(2, 2, figsize=(10, 10)) 
     axes[0, 0].imshow(image)
